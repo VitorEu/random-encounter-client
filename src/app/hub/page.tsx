04ed99a011e9814fe2d57c7677796a2b39a6e88c
VITORPASSOS <vitoreugenio02@gmail.com>
2023-07-28 17:43:53 -0300

    interface and navegability fixes

--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -1,9 +1,23 @@
+'use client'
 import { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useContext, useEffect, useState } from "react";
 import { getAPIClient } from "../_api/base.apiClient";
+import { useRouter } from "next/navigation";
+import { success } from "../_utils/toast";
+import { AuthContext } from "../contexts/AuthContext";
 
 export default function Page() {
+
+    const { isAuthenticated } = useContext(AuthContext)
+
+    const Router = useRouter();
+    useEffect(() => {
+        const { 're.token': token } = parseCookies()
+        if (!isAuthenticated) {
+            Router.push("/")
+        }
+    }, []);
 
     return (
         <main className="bg-giant-dark bg-bottom p-[2.80rem] flex flex-row">
